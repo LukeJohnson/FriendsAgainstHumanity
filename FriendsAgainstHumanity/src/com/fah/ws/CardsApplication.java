1798377958f596bed1bdd8353bf28f4f8bc2df9f
jmguillemette <jmguillemette@gmail.com>
2014-02-01 17:57:01 -0500

    adding vote service.

--- FriendsAgainstHumanity/src/com/fah/ws/CardsApplication.java
+++ FriendsAgainstHumanity/src/com/fah/ws/CardsApplication.java
@@ -7,6 +7,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 import com.fah.service.CardService;
+import com.fah.service.VoteService;
 
 @ApplicationPath("/fah")
 public class CardsApplication extends Application{
@@ -14,6 +15,7 @@ public class CardsApplication extends Application{
 	public Set<Class<?>> getClasses() {
 		Set<Class<?>> s = new HashSet<Class<?>>();
 		s.add(CardService.class);
+		s.add(VoteService.class);
 		s.add(JsonMessageBodyWriter.class);
 		s.add(JsonMessageBodyReader.class);
 		return s;
