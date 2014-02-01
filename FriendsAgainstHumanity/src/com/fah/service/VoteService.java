622d234dc0e90951127fd10fc9ad9847f664d6a0
jmguillemette <jmguillemette@gmail.com>
2014-02-01 18:23:32 -0500

    fix for broken code.

--- FriendsAgainstHumanity/src/com/fah/service/VoteService.java
+++ FriendsAgainstHumanity/src/com/fah/service/VoteService.java
@@ -2,12 +2,15 @@ package com.fah.service;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.sql.Connection;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
+
+import com.fah.db.DatabaseUtil;
 
 @Path("/vote")
 public class VoteService {
@@ -21,7 +24,8 @@ public class VoteService {
 	@PUT
 	@Path("{cardId}")
 	public Response castVote(boolean upVote)throws URISyntaxException{
-		Connection conn 
+		Connection conn = DatabaseUtil.getConnection();
+		DatabaseUtil.closeConnection(conn);
 		ResponseBuilder respBuiltder = Response.created(new URI("Vote Casted"));
 		return respBuiltder.build();
 	}
